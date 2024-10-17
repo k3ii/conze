f331f286c053ad753d39ed68a83da92e99d2bacb
k3ii <jainr@ringier.co.za>
2024-10-17 17:23:01 +0400

    feat(cli): Add subcommand config and list

--- src/cli.rs
+++ src/cli.rs
@@ -13,8 +13,7 @@ pub fn cli() -> Command {
                     Arg::new("country")
                         .long("country")
                         .short('c')
-                        .help("Specify country (e.g., MU for Mauritius, SA for South Africa)")
-                        .default_value("MU"),
+                        .help("Specify country (e.g., MU for Mauritius, ZA for South Africa)"),
                 ),
         )
         .subcommand(
@@ -27,7 +26,34 @@ pub fn cli() -> Command {
                         .long("compare")
                         .short('c')
                         .aliases(["cmp", "cpm"])
-                        .help("Compare holidays with another country (e.g., SA for South Africa)"),
+                        .help("Compare holidays with another country (e.g., ZA for South Africa)"),
+                ),
+        )
+        .subcommand(
+            Command::new("config")
+                .arg(
+                    Arg::new("default-country")
+                        .long("default-country")
+                        .value_name("COUNTRY_CODE")
+                        .ignore_case(true)
+                        .help("Sets the default country"),
+                )
+                .subcommand(Command::new("show").about("Displays the current configuration")),
+        )
+        .subcommand(
+            Command::new("list")
+                .about("List holidays for a specific country and year")
+                .arg(
+                    Arg::new("country")
+                        .long("country")
+                        .short('c')
+                        .help("Specify country (e.g., MU for Mauritius, ZA for South Africa)"),
+                )
+                .arg(
+                    Arg::new("year")
+                        .short('y')
+                        .long("year")
+                        .help("Specify the year"),
                 ),
         )
 }
